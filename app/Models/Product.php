28ba775d32a81a50f8af289918dc1e66b64f51e2
el4l3x <davidlopez.fya@gmail.com>
2024-03-08 06:33:01 -0400

    update fecha tras scrap

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -30,7 +30,8 @@ class Product extends Model
                 $web->go($product->gfc);
                 $string = $web->filter("//div[@class='current-price']//span[@class='price_with_tax price_pvp']")->text();
                 $string = Str::remove('€', $string);
-                $product->gfc_price = floatval(Str::replace(',', '.', $string));                
+                $product->gfc_price = floatval(Str::replace(',', '.', $string));
+                $product->save();
             }
 
             if ($product->climahorro_price == null || $product->updated_at < now()->toDateString()) {
@@ -39,6 +40,7 @@ class Product extends Model
                 $string = Str::remove('€', $string);
                 $product->climahorro_price = floatval(Str::replace(',', '.', $string));
                 $product->climahorro_percent = number_format((((($product->gfc_price - $product->climahorro_price)/$product->gfc_price))*100)*-1, 2);
+                $product->save();
             }
 
             if ($product->ahorraclima_price == null || $product->updated_at < now()->toDateString()) {
@@ -47,6 +49,7 @@ class Product extends Model
                 $string = Str::remove('€', $string);
                 $product->ahorraclima_price = floatval(Str::replace(',', '.', $string));
                 $product->ahorraclima_percent = number_format((((($product->gfc_price - $product->ahorraclima_price)/$product->gfc_price))*100)*-1, 2);
+                $product->save();
             }
 
             if ($product->expertclima_price == null || $product->updated_at < now()->toDateString()) {
@@ -55,6 +58,7 @@ class Product extends Model
                 $string = Str::remove('€', $string);
                 $product->expertclima_price = floatval(Str::replace(',', '.', $string));
                 $product->expertclima_percent = number_format((((($product->gfc_price - $product->expertclima_price)/$product->gfc_price))*100)*-1, 2);
+                $product->save();
             }
 
             if ($product->tucalentadoreconomico_price == null || $product->updated_at < now()->toDateString()) {
@@ -63,9 +67,8 @@ class Product extends Model
                 $string = Str::remove('€', $string);
                 $product->tucalentadoreconomico_price = floatval(Str::replace(',', '.', $string));
                 $product->tucalentadoreconomico_percent = number_format((((($product->gfc_price - $product->tucalentadoreconomico_price)/$product->gfc_price))*100)*-1, 2);
+                $product->save();
             }
-
-            $product->save();
             
         });
     }
