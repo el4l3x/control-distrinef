f2653ecdf02f54741b142b806c732c85101513cd
el4l3x <davidlopez.fya@gmail.com>
2024-03-11 09:14:10 -0400

    test

--- app/Http/Controllers/GfcController.php
+++ app/Http/Controllers/GfcController.php
@@ -77,8 +77,8 @@ class GfcController extends Controller
                 ->whereBetween('orders.date_add', [$start, $end]);
         })
         ->join('category_product', function (JoinClause $joinClause) {
-            $joinClause->on('product.id_product', '=', 'category_product.id_product')
-                ->where('category_product.id_category', 770);
+            $joinClause->on('product.id_product', '=', 'category_product.id_product');
+                /* ->where('category_product.id_category', 770); */
         })        
         ->select(
             'product.id_product',
