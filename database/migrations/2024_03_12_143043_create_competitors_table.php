753a7906893bbd5f26a1dd8face182978718aeac
el4l3x <davidlopez.fya@gmail.com>
2024-03-12 10:51:32 -0400

    Model Product + Competitors & migrations

--- database/migrations/2024_03_12_143043_create_competitors_table.php
+++ database/migrations/2024_03_12_143043_create_competitors_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('competitors', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->string('url');
-            $table->decimal('precio');
+            $table->string('filtro');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('competitors');
     }
 };
