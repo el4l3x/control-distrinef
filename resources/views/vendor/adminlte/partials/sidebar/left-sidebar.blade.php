7bad0b5fb8ebf780108b56e0af796d60aa24ab2b
el4l3x <davidlopez.fya@gmail.com>
2024-03-19 12:18:07 -0400

    Cambiar foto de user en sidebar

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -20,8 +20,8 @@
                 @endif>
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel d-flex mt-3 pb-3 mb-3">
-                    <div class="info">
-                        <i class="far fa-user fa-lg" style="color: #C2c7c0;"></i>
+                    <div class="image">
+                        <img src="{{ asset('img/users/porto.png') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ Auth::user()->name }}</a>
