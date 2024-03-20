771a7c0cf38359f01484b14eae6a85e564a52225
el4l3x <davidlopez.fya@gmail.com>
2024-03-20 16:50:53 -0400

    colores a info box dashboard

--- resources/views/gfc/dashboard.blade.php
+++ resources/views/gfc/dashboard.blade.php
@@ -30,29 +30,29 @@
     <div class="row">
         <div class="col-md-3">
             {{-- Pedidos Entrados --}}
-            <x-adminlte-small-box title="{{ $pedidosEntrados }}" text="Pedidos Entrados" icon="far fa-chart-bar" theme="info" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="{{ $pedidosEntrados }}" text="Pedidos Entrados" icon="far fa-chart-bar" theme="primary" url="#" url-text="Mas Información"/>
         </div>
         
         <div class="col-md-3">
             {{-- Importe Facturado --}}
-            <x-adminlte-small-box title="{{ $importeFacturado }}" text="Importe Facturado" icon="far fa-chart-bar" theme="warning" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="{{ $importeFacturado }}" text="Importe Facturado" icon="far fa-chart-bar" theme="primary" url="#" url-text="Mas Información"/>
         </div>
 
         <div class="col-md-3">
             {{-- Carritos Totales --}}
-            <x-adminlte-small-box title="{{ $carritosTotales }}" text="Carritos Totales" icon="fas fa-shopping-cart" theme="success" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="{{ $carritosTotales }}" text="Carritos Totales" icon="fas fa-shopping-cart" theme="primary" url="#" url-text="Mas Información"/>
         </div>
 
         <div class="col-md-3">
             {{-- Carritos Clientes --}}            
-            <x-adminlte-small-box title="{{ $carritosClientes }}" text="Carritos Clientes" icon="fas fa-shopping-cart" theme="indigo" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="{{ $carritosClientes }}" text="Carritos Clientes" icon="fas fa-shopping-cart" theme="primary" url="#" url-text="Mas Información"/>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-3">
             {{-- Productos Activos Hoy --}}
-            <x-adminlte-small-box title="{{ $productsAct }}" text="Productos Activos" icon="fas fa-shopping-bag" theme="info" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="{{ $productsAct }}" text="Productos Activos" icon="fas fa-shopping-bag" theme="warning" url="#" url-text="Mas Información"/>
         </div>
         
         <div class="col-md-3">
@@ -62,34 +62,34 @@
 
         <div class="col-md-3">
             {{-- Combinaciones Activas --}}
-            <x-adminlte-small-box title="{{ $productsUpd }}" text="Combinaciones Activas" icon="fas fa-shopping-bag" theme="success" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="{{ $productsUpd }}" text="Combinaciones Activas" icon="fas fa-shopping-bag" theme="warning" url="#" url-text="Mas Información"/>
         </div>
 
         <div class="col-md-3">
             {{-- Productos Productos Nunca Vendidos --}}            
-            <x-adminlte-small-box title="16.765*" text="Productos Nunca Vendidos" icon="fas fa-shopping-bag" theme="indigo" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="16.765*" text="Productos Nunca Vendidos" icon="fas fa-shopping-bag" theme="warning" url="#" url-text="Mas Información"/>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-3">
             {{-- Matcheados con Distribase --}}
-            <x-adminlte-small-box title="12.355*" text="Matcheados con Distribase" icon="fas fa-shopping-bag" theme="olive" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="12.355*" text="Matcheados con Distribase" icon="fas fa-shopping-bag" theme="success" url="#" url-text="Mas Información"/>
         </div>
         
         <div class="col-md-3">
             {{-- NO Matcheados con Distribase --}}
-            <x-adminlte-small-box title="10.325*" text="NO Matcheados con Distribase" icon="fas fa-shopping-bag" theme="orange" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="10.325*" text="NO Matcheados con Distribase" icon="fas fa-shopping-bag" theme="success" url="#" url-text="Mas Información"/>
         </div>
 
         <div class="col-md-3">
             {{-- Productos con Stock en CSV --}}
-            <x-adminlte-small-box title="6.780*" text="Productos con Stock en CSV" icon="fas fa-shopping-bag" theme="maroon" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="6.780*" text="Productos con Stock en CSV" icon="fas fa-shopping-bag" theme="success" url="#" url-text="Mas Información"/>
         </div>
 
         <div class="col-md-3">
             {{-- Productos con Neto en CSV --}}            
-            <x-adminlte-small-box title="5.890*" text="Productos con Neto en CSV" icon="fas fa-shopping-bag" theme="purple" url="#" url-text="Mas Información"/>
+            <x-adminlte-small-box title="5.890*" text="Productos con Neto en CSV" icon="fas fa-shopping-bag" theme="success" url="#" url-text="Mas Información"/>
         </div>
     </div>
 
